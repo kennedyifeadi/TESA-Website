44ea0b5f5f34551fecc7492796eee05b924b31f3
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-15 19:34:17 +0100

    Refactor HeroSection component and add DynamicBackground, Button, and styling

--- Frontend/src/components/HeroSection.jsx
+++ Frontend/src/components/HeroSection.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import { DynamicBackground } from './DynamicBackground'
+import Background from '../assets/images/HeroSectionBackground.jpg'
+import { Button } from './UI/Button'
 
 export const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div className='flex h-[100dvh] w-full relative px-1 border-l-[1px] border-[#07101B] border-r-[1px]'>
+      <DynamicBackground imageUrl={Background}/>
+      <div className='flex flex-col h-full items-center z-10'>
+        <div>
+          <h1></h1>
+        </div>
+        <div>
+          <h3>
+
+          </h3>
+        </div>
+        <div>
+          <p>
+
+          </p>
+        </div>
+        <div>
+          <Button/>
+        </div>
+      </div>
+      <div className='flex h-full items-end z-10'>
+      </div>
+    </div>
   )
 }
---
+++ Frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HeroSection } from '../components/HeroSection'
+
+export const LandingPage = () => {
+  return (
+    <div className='w-full h-max flex flex-col'>
+        <HeroSection/>
+    </div>
+  )
+}
