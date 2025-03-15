4ac8ab6f659d3cb11847e2cd2bc86604f12d1eea
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-16 00:13:23 +0100

    Refactor App component to include NavContextProvider and DynamicBackground component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter } from "react-router-dom"
 import { NavBar } from "./components/Nav"
 import { AnimatedRoutes } from "./routes/AnimatedRoute"
+import { NavContextProvider } from "./context/NavContext"
 
 function App() {
 
   return (
     <BrowserRouter>
+    <NavContextProvider>
     <div className="flex w-full h-max flex-col relative">
         <NavBar/>
         <AnimatedRoutes/>
     </div>
+    </NavContextProvider>
     </BrowserRouter>
   )
 }
