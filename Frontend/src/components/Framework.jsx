6e76cb316b6e079d7eebcb0b6b9bde96fb7c0f75
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-16 08:31:34 +0100

    Refactor LandingPage component to include Framework section

--- Frontend/src/components/Framework.jsx
+++ Frontend/src/components/Framework.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { FrameworkObject } from './FrameworkObject'
+import arrow from '../assets/gifs/arrow.gif'
 
-export const framework = () => {
+export const Framework = () => {
   return (
-    <div>framework</div>
+    <div className='relative gap-4 w-full h-[100dvh] items-center flex flex-col' style={{ background: "linear-gradient(91.03deg, #000000 -13.29%, #392B1A 122.75%)" }}>
+      <div className='w-full h-max mt-10 flex justify-center'>
+        <h1 className='bg-gradient-to-r from-[#007AFF] to-[#FA8F21] bg-clip-text text-transparent text-5xl'>THE TESA Framework</h1>
+      </div>
+      <div className='h-max mt-4 w-[392px] gap-4 flex flex-wrap' >
+        {
+          FrameworkObject.map((item, index)=>{
+            return (
+              <div key={index} className={`w-[120px] h-[120px]   ${item.id === 5 ? "bg-transparent rounded-full " : "bg-white rounded-md"} flex justify-center items-center`}>
+                <img src={item.image} alt="" className={` ${item.id === 5 ? "w-full h-full object-cover" : "w-[60%]"}`} />
+              </div>
+            )
+          })
+        }
+      </div>
+      <div className='w-[100px] h-[70px]'>
+        <img src={arrow} alt="" className='w-full h-full' />
+      </div>
+    </div>
   )
 }
--- Frontend/src/pages/LandingPage.jsx
+++ Frontend/src/pages/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { HeroSection } from '../components/HeroSection'
+import { Framework } from '../components/framework'
 
 export const LandingPage = () => {
   return (
     <div className='w-full h-max flex flex-col'>
         <HeroSection/>
+        <Framework/>
     </div>
   )
 }
