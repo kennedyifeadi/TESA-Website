7dff0a6ee35e0829c1c782084c34462100ec6f8f
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-17 00:06:05 +0100

    Refactor component imports in LandingPage

--- Frontend/src/pages/LandingPage.jsx
+++ Frontend/src/pages/LandingPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { HeroSection } from '../components/HeroSection'
-import { Framework } from '../components/framework'
-import { Executives } from '../components/executives'
+import { Framework } from '../components/Framework'
 import { Sponsorship } from '../components/Sponsorship'
 import { About } from '../components/About'
 import { TesaPress } from '../components/TesaPress'
 import { Newsletter } from '../components/Newsletter'
 import { Location } from '../components/Location'
 import { Socials } from '../components/Socials'
-import { Footer } from '../components/footer'
+import { Footer } from '../components/Footer'
+import { Executives } from '../components/Executives'
 
 export const LandingPage = () => {
   return (
