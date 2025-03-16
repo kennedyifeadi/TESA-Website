50fd57eb812bdacaa6b35ea3e8585db6230df3d6
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-16 14:18:15 +0100

    Refactor Sponsorship component to adjust height and styling

--- Frontend/src/components/Sponsorship.jsx
+++ Frontend/src/components/Sponsorship.jsx
@@ -3,11 +3,11 @@ import { Carousel } from './SponsoredCarousel'
 
 export const Sponsorship = () => {
   return (
-    <div className='w-full h-[100dvh] flex flex-col px-8 py-10 gap-4'>
-      <div className='w-full h-[10%] flex justify-end items-center'>
+    <div className='w-full h-[90dvh] flex flex-col px-8 py-10 gap-4'>
+      <div className='w-full h-[11%] flex justify-end items-center'>
         <span className='text-white bg-[#00F652] rounded-lg h-[80%] w-[150px] font-medium flex justify-center items-center text-[18px]'>Sponsored</span>
       </div>
-      <div className='w-full h-[80%]'>
+      <div className='w-full h-[85%]'>
         <Carousel/>
       </div>
     </div>
