0c9e8d0e9a88cbdb07afcb334e59c74fb98cfefd
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-16 11:39:31 +0100

    Refactor LandingPage component to include Executives section

--- Frontend/src/pages/LandingPage.jsx
+++ Frontend/src/pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { HeroSection } from '../components/HeroSection'
 import { Framework } from '../components/framework'
+import { Executives } from '../components/executives'
 
 export const LandingPage = () => {
   return (
     <div className='w-full h-max flex flex-col'>
         <HeroSection/>
         <Framework/>
+        <Executives/>
     </div>
   )
 }
