7529ae35836b14c7112a0258fd7886a38437f08c
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-16 11:39:52 +0100

    Refactor ExecutivesSocials and ExecutivesBtn components

--- Frontend/src/components/UI/ExecutivesBtn.jsx
+++ Frontend/src/components/UI/ExecutivesBtn.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const ExecutivesBtn = () => {
+export const ExecutivesBtn = ({text}) => {
   return (
-    <div>ExecutivesBtn</div>
+    <div className='flex justify-center items-center rounded-[8px] w-[50%] h-full p-[2px] ' style={{background: "linear-gradient(90deg, #007AFF 0%, #FA8F21 100%)"}}>
+      <div className='w-full h-full flex justify-center items-center rounded-[6px] bg-[#392B1A]'> {text}</div>
+    </div>
   )
 }
--- Frontend/src/components/UI/ExecutivesSocials.jsx
+++ Frontend/src/components/UI/ExecutivesSocials.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export const ExecutivesSocials = () => {
+export const ExecutivesSocials = ({logo, link}) => {
   return (
-    <div>ExecutivesSocials</div>
+    <div className='h-full flex justify-center items-center bg-transparent flex-1'>
+      <a href={link} className='w-full h-full'>
+        <img src={logo} alt="" className='w-full h-full object-contain' />
+      </a>
+    </div>
   )
 }
