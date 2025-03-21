fd6aa8f5238cbe59541128a6164c18adf5fa69e0
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-21 12:52:11 +0100

    Refactor Framework component to optimize rendering and add animation

--- Frontend/src/components/Framework.jsx
+++ Frontend/src/components/Framework.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FrameworkObject } from './FrameworkObject';
 import arrow from '../assets/gifs/arrow.gif';
+import { motion } from 'framer-motion';
 
 export const Framework = () => {
   return (
@@ -10,41 +11,39 @@ export const Framework = () => {
         background: 'linear-gradient(91.03deg, #000000 -13.29%, #392B1A 122.75%)',
       }}
     >
-      {/* Top Left Line */}
       <div
         className="absolute top-[12.3vh] h-[2px] left-[max(-2vw,-1%)] rotate-[17deg]"
         style={{ background: 'linear-gradient(90deg, #007AFF 0%, #FA8F21 100%)', width: "clamp(30vw, 40vw, 45vw)" }}
       ></div>
 
-      {/* Top Right Line */}
       <div
         className="absolute top-[12.3vh] h-[2px] right-[max(-2vw,-1%)] rotate-[-17deg]"
         style={{ background: 'linear-gradient(90deg, #007AFF 0%, #FA8F21 100%)', width: "clamp(40vw, 40vw, 45vw)" }}
       ></div>
 
-      {/* Bottom Left Line */}
       <div
         className="absolute bottom-[15.5vh] h-[2px] left-[max(-2vw,-2.5%)] rotate-[-22.5deg]"
         style={{ background: 'linear-gradient(90deg, #007AFF 0%, #FA8F21 100%)', width: "clamp(40vw, 40vw, 45vw)" }}
       ></div>
 
-      {/* Bottom Right Line */}
       <div
         className="absolute bottom-[15.5vh] bg-white h-[2px] right-[max(-2vw,-2.5%)] rotate-[22.5deg]"
         style={{ background: 'linear-gradient(90deg, #007AFF 0%, #FA8F21 100%)', width: "clamp(40vw, 40vw, 45vw)" }}
       ></div>
 
-      {/* Title */}
       <div className="w-full h-max flex justify-center">
         <h1 className="bg-gradient-to-r from-[#007AFF] to-[#FA8F21] bg-clip-text text-transparent text-5xl">
           THE TESA Framework
         </h1>
       </div>
 
-      {/* Boxes */}
       <div className="h-max mt-4 w-[40%] gap-4 flex flex-wrap justify-between z-10">
         {FrameworkObject.map((item, index) => (
-          <div
+          <motion.div
+            initial = {{opacity: 0, scale: 0}}
+            whileInView={{opacity: 1, scale: 1}}
+            transition={{duration: 0.8, delay: item.id / 10}}
+            viewport={{once: true}}
             key={index}
             className={`w-[30%] h-[150px] flex justify-center items-center ${
               item.id === 5 ? 'bg-transparent rounded-full' : 'bg-white rounded-md'
@@ -55,11 +54,10 @@ export const Framework = () => {
               alt=""
               className={`${item.id === 5 ? 'w-full h-full object-cover' : 'w-[60%]'}`}
             />
-          </div>
+          </motion.div>
         ))}
       </div>
 
-      {/* Arrow */}
       <div className="w-[100px] h-[70px]">
         <img src={arrow} alt="" className="w-full h-full" />
       </div>
