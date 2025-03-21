ca55fccab3c374957f32bb136a81ece1dc31a267
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-21 16:25:11 +0100

    refactor whysponsor component

--- Frontend/src/pages/Sponsorships.jsx
+++ Frontend/src/pages/Sponsorships.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { SponsorshipHeroSection } from '../components/SponsorshipHeroSection'
+import { WhySponsor } from '../components/WhySponsor'
 
 export const Sponsorships = () => {
   return (
     <div className='w-full h-maax flex flex-col relative'>
       <SponsorshipHeroSection/>
+      <WhySponsor/>
     </div>
   )
 }
