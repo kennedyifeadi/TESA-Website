e660624151ffe687e5283d1e8bfcf2fa1e646851
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-21 09:39:59 +0100

    Refactor PageLoading component to improve loading experience and fix memory leak

--- Frontend/src/components/PageLoading.jsx
+++ Frontend/src/components/PageLoading.jsx
@@ -1,33 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { DynamicBackground } from './DynamicBackground';
 import Background from "../assets/images/HeroSectionBackground.jpg";
+import TesaLogo from "../assets/images/tesaLogo.png"
+import { NavContext } from '../context/NavContext';
 
 
 export const PageLoading = () => {
     const [progress, setProgress] = useState(0)
+    const {isLoading, setIsLoading} = useContext(NavContext)
 
     useEffect(() => {
         const interval = setInterval(() => {
           setProgress((oldProgress) => {
             if (oldProgress >= 100) {
               clearInterval(interval);
-              setIsLoading(false);
               return 100;
             }
-            return oldProgress + 2; // Increase progress gradually
+            return oldProgress + 2; 
           });
-        }, 100); // Runs every 100ms
-    
-        return () => clearInterval(interval);
-      }, []);
+        }, 100);
+
+        const timeout = setTimeout(() => {
+            setIsLoading(false);
+          }, 5000); 
+      
+          return () => {
+            clearInterval(interval);
+            clearTimeout(timeout);
+          };
+      }, [setIsLoading]);
   return (
-    <div className='w-screen h-screen relative flex flex-col'>
+    <div className={`w-full h-screen absolute z-40 flex flex-col justify-center items-center duration-1000 transition-all ease-in-out ${isLoading ? "top-0 " : "top-[-100%]"}`}>
         <DynamicBackground imageUrl={Background}/>
-        <div className='w-[40%] h-[50%]'>
-            <img src="" alt="" className='w-full h-full object-cover' />
+        <div className='w-[30%] h-[40%] z-10'>
+            <img src={TesaLogo} alt="" className='w-full h-full object-contain' />
         </div>
-        <div className="w-[40%] h-[10px] rounded-full overflow-hidden bg-transparent">
-            <div className="h-full w-0 rounded-full transition-all duration-100 ease-linear bg-black" style={{ width: `${progress}%` }}></div>
+        <div className="w-[20%] h-[10px] rounded-full overflow-hidden bg-transparent z-10">
+            <div className="h-full w-0 rounded-full transition-all duration-100 ease-linear" style={{ width: `${progress}%`, background: "linear-gradient(90deg, #007AFF 22.22%, #FFF8D6 56.13%, #CF2317 90.48%)" }}></div>
         </div>
     </div>
   )
