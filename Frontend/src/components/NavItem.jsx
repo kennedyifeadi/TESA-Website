7c69105eb7cf4008558852c30465999c011f5031
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-21 09:39:47 +0100

    Refactor NavItem component to remove unused code and dependencies ``

--- Frontend/src/components/NavItem.jsx
+++ Frontend/src/components/NavItem.jsx
@@ -1,13 +1,8 @@
-import React, { useContext } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
-import { NavContext } from "../context/NavContext";
 import { motion } from "framer-motion";
 
 export const NavItem = ({ NavUrl, title, ids }) => {
-  const { setId } = useContext(NavContext);
-  const handleClick = () => {
-    setId(ids);
-  };
 
   return (
     <div
@@ -21,7 +16,6 @@ export const NavItem = ({ NavUrl, title, ids }) => {
             animate={{ backgroundColor: isActive ? "#392B1A" : "", color: isActive ? "#fff" : "", borderBottom: isActive ? "2px solid #007AFF " : ""}}
             transition={{ duration: 0.3 }}
             className="w-full h-full flex justify-center items-center hover:bg-[#392B1A] text-[#000] hover:text-white duration-500 ease-in-out relative before:absolute before:w-full before:bottom-0 hover:before:h-[2px] before:h-0 before:bg-[#007AFF]"
-            onClick={handleClick}
           >
             <span
               className="text-[24px] font-medium"
