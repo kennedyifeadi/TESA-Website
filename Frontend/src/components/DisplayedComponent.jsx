87990a450e2ffd82ec805918fc7cc7f53a23c032
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-21 12:44:41 +0100

    Refactor DisplayedComponent and HeroSection components

--- Frontend/src/components/DisplayedComponent.jsx
+++ Frontend/src/components/DisplayedComponent.jsx
@@ -1,19 +1,27 @@
-import React, { useContext } from 'react'
-import { PageLoading } from './PageLoading'
-import { NavBar } from './Nav'
-import { AnimatedRoutes } from '../routes/AnimatedRoute'
-import { NavContext } from '../context/NavContext'
+import React, { useContext, useState, useEffect } from 'react';
+import { PageLoading } from './PageLoading';
+import { NavBar } from './Nav';
+import { AnimatedRoutes } from '../routes/AnimatedRoute';
+import { NavContext } from '../context/NavContext';
 
 export const DisplayedComponent = () => {
-  const {isLoading} = useContext(NavContext)
+  const { isLoading } = useContext(NavContext);
+  const [showHero, setShowHero] = useState(false);
+
+  useEffect(() => {
+    if (!isLoading) {
+        setShowHero(true);
+    }
+  }, [isLoading]);
 
   return (
     <div>
-          <PageLoading/>
-          <div className={`${isLoading ? "hidden" : "flex"} h-max flex-col relative overflow-x-hidden`}>
-            <NavBar/>
-            <AnimatedRoutes/>
-         </div>
+      <PageLoading />
+      <div className={`${isLoading ? 'hidden' : 'flex'} h-max flex-col relative overflow-x-hidden`}>
+        <NavBar />
+        {showHero && <AnimatedRoutes />}
+        
+      </div>
     </div>
-  )
-}
+  );
+};
