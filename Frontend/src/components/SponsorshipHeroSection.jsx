812774ea4dc872f222cb54a6ae7027469f341ae3
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-23 08:38:59 +0100

    Refactor SponsorshipHeroSection component to remove unnecessary border styling

--- Frontend/src/components/SponsorshipHeroSection.jsx
+++ Frontend/src/components/SponsorshipHeroSection.jsx
@@ -9,7 +9,7 @@ export const SponsorshipHeroSection = () => {
   return (
     <div className='w-full h-screen flex items-end px-8 relative  z-10'>
         <DynamicBackground imageUrl={Background}/>
-        <div className='border-r-[1px] border-l-[1px] w-full h-[90%] pt-4 z-10'>
+        <div className='w-full h-[90%] pt-4 z-10'>
             <div className='w-full h-[30%] relative flex flex-col justify-center items-center'>
                 <DynamicBackground imageUrl={DummyBG}/>
                 <h1 className='w-full text-center text-5xl text-white z-10' style={{ fontFamily: '"Aldrich", sans-serif' }}>SPONSORSHIP</h1>
