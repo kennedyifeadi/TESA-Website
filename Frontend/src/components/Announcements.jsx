ed43a0bd17b361b0b6f69de94dc5af664970921c
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-23 16:13:13 +0100

    Refactor components and add new ones for Announcements, Calendar, Events, and Gallery

---
+++ Frontend/src/components/Announcements.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Gallery } from './Gallery'
+
+export const Announcements = () => {
+  return (
+    <div>
+        <div>
+            <Gallery/>
+        </div>
+    </div>
+  )
+}
