07483e6729b5a1269e7eee03e47fbd18545ba440
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-23 08:39:12 +0100

    Refactor Sponsorships page to add Sponsors component

--- Frontend/src/pages/Sponsorships.jsx
+++ Frontend/src/pages/Sponsorships.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { SponsorshipHeroSection } from '../components/SponsorshipHeroSection'
 import { WhySponsor } from '../components/WhySponsor'
+import { Sponsors } from '../components/Sponsors'
 
 export const Sponsorships = () => {
   return (
     <div className='w-full h-maax flex flex-col relative'>
       <SponsorshipHeroSection/>
       <WhySponsor/>
+      <Sponsors/>
     </div>
   )
 }
