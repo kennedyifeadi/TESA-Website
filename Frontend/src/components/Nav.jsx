3cbad74febbd9d9c6e1c035ef50078563657943c
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-27 03:32:05 +0100

    Refactor Nav component for improved responsiveness and styling

--- Frontend/src/components/Nav.jsx
+++ Frontend/src/components/Nav.jsx
@@ -35,7 +35,7 @@ export const NavBar = () => {
           <img src={notification} alt=""  className='absolute right-[-40px]' />
         </div>
       </div>
-      <div className={`bg-gradient-to-r from-[#007AFF] to-[#FA8F21] md:bg-none flex md:flex-row flex-col h-screen justify-center ${dialogClicked ? "right-0" : "right-[-500px]"} w-[50%] ${searchClicked ? "md:w-[70%]": "md:w-[80%]"} duration-500 transition-all ease-in-out md:h-full items-start md:items-center border-b-[#07101B] border-b md:right-0 md:relative absolute`}>
+      <div className={`bg-gradient-to-r from-[#007AFF] to-[#FA8F21] md:bg-none flex md:flex-row flex-col h-screen justify-center ${dialogClicked ? "right-0" : "right-[-500px]"} w-[80%] ${searchClicked ? "md:w-[70%]": "md:w-[80%]"} duration-500 transition-all ease-in-out md:h-full items-start md:items-center border-b-[#07101B] border-b md:right-0 md:relative absolute`}>
       <IoMdClose className="flex md:hidden text-white absolute top-4 right-0 cursor-pointer text-5xl" onClick={handleDialogClick}/>
         {
           NavItems.map((nav, index)=>{
@@ -47,12 +47,12 @@ export const NavBar = () => {
           })
         }
       </div>
-      <div className={`flex md:right-0 h-full w-[50%] justify-center  ${searchClicked ? "md:w-[20%]": "md:w-[10%]"} items-center duration-500 transition-all ease-in-out absolute ${dialogClicked ? "right-0" : "right-[-500px]"}  top-[140%] md:top-0 md:relative`}>
+      <div className={`flex md:right-0 h-full w-[80%] justify-center  ${searchClicked ? "md:w-[20%]": "md:w-[10%]"} items-center duration-500 transition-all ease-in-out absolute ${dialogClicked ? "right-0" : "right-[-500px]"}  top-[30%] md:top-0 md:relative`}>
         <div className={`h-full hidden md:flex items-center w-[40%] ${searchClicked ? "md:w-[20%]": "md:w-[40%]"} duration-500 transition-all ease-in-out cursor-pointer`}>
           <img src={UserIcon} alt="" />
         </div>
         <div className={`h-full flex items-center w-[80%] ${searchClicked ? "md:w-[80%]": "md:w-[60%]"} duration-500 transition-all ease-in-out relative cursor-pointer`}>
-          <input type="text" className={`${searchClicked ? "md:w-full md:h-[95%] md:border-2 md:p-2" : "md:w-0 md:h-0"} w-full p-2 h-[70%] bg-white md:bg-transparent outline-none duration-500 rounded-md transition-all ease-in-out  md:border-[#007AFF]`} />
+          <input type="text" className={`${searchClicked ? "md:w-full md:h-[95%] md:border-2 md:p-2" : "md:w-0 md:h-0"} w-full p-2 h-[40%] bg-white md:bg-transparent outline-none duration-500 rounded-md transition-all ease-in-out  md:border-[#007AFF]`} />
           <img src={SearcgIcon} alt="" className={`absolute ${searchClicked ? "md:w-10 md:h-10 ": "md:-full md:h-full"} w-10 h-10 object-cover right-0 duration-500 transition-all ease-in-out`} onClick={handleSearchClick}/>
         </div>
       </div>
