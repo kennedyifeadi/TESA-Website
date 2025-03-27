c83397f7934d7acde9d1465764a2009b55b010e9
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-27 17:55:41 +0100

    Refactor Socials component for improved responsiveness and styling

--- Frontend/src/components/UI/SocialsCard.jsx
+++ Frontend/src/components/UI/SocialsCard.jsx
@@ -4,17 +4,17 @@ export const SocialsCard = ({text, icon, hoverIcon}) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div className='h- full flex justify-center items-center flex-1 gap-2 px-4 hover:bg-[#392B1A] duration-500 ease-in-out'
+    <div className='h-full flex justify-center items-center flex-1 px-4 hover:bg-[#392B1A] duration-500 ease-in-out'
     onMouseEnter={() => setIsHovered(true)}
     onMouseLeave={() => setIsHovered(false)}
     >
-      <span className={`${isHovered ? "translate-y-[-20px] text-white" : "translate-y-0"} text-black text-2xl font-medium duration-500 ease-in-out transition-all`} style={{fontFamily:'"Poppins", sans-serif'}}>{text}</span>
+      <span className={`${isHovered ? "translate-y-[-20px] text-white" : "translate-y-0"} text-black text-[10px] md:text-2xl font-medium duration-500 ease-in-out transition-all`} style={{fontFamily:'"Poppins", sans-serif'}}>{text}</span>
       {
         isHovered ? (
-          <img src={hoverIcon} alt="" className='w-8 h-8 translate-y-[-10px]'/>
+          <img src={hoverIcon} alt="" className='w-4 md:w-8 h-4 md:h-8 translate-y-[-10px]'/>
           
         ) : (
-          <img src={icon} alt="" className='w-8 h-8' />
+          <img src={icon} alt="" className='w-4 md:w-8 h-4 md:h-8' />
         )
       }
     </div>
