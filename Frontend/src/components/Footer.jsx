1d810ecbb5359164b3bd47b413a8250c889cf8ad
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-27 17:46:33 +0100

    Refactor Footer component for improved responsiveness and styling

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='w-full h-[10dvh] hidden justify-between items-center bg-[#101010] px-8'>
-      <span className='text-[15px] text-white font-medium' style={{fontFamily:'"Poppins", sans-serif'}}>Copywright TESA 2025</span>
-      <span className='text-[15px] text-white font-medium' style={{fontFamily:'"Poppins", sans-serif'}}>JFK x Emmanuel</span>
+    <div className='w-full h-[10dvh] flex justify-between items-center bg-[#101010] px-2 md:px-8'>
+      <span className='text-[10px] md:text-[15px] text-white font-medium' style={{fontFamily:'"Poppins", sans-serif'}}>Copywright TESA 2025</span>
+      <span className='text-[10px] md:text-[15px] text-white font-medium' style={{fontFamily:'"Poppins", sans-serif'}}>JFK x Emmanuel</span>
     </div>
   )
 }
