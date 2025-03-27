993587802aff2eb491d0a4904140ce335e5441e9
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-27 02:48:32 +0100

    Refactor Framework component to improve styling and responsiveness

--- Frontend/src/components/Framework.jsx
+++ Frontend/src/components/Framework.jsx
@@ -6,38 +6,38 @@ import { motion } from 'framer-motion';
 export const Framework = () => {
   return (
     <div
-      className="relative gap-4 pt-10 w-full h-[120dvh] flex flex-col items-center"
+      className="relative gap-4 pt-10 w-full h-max md:h-[120dvh] flex flex-col items-center"
       style={{
         background: 'linear-gradient(91.03deg, #000000 -13.29%, #392B1A 122.75%)',
       }}
     >
       <div
-        className="absolute top-[12.3vh] h-[2px] left-[max(-2vw,-1%)] rotate-[17deg]"
+        className="absolute md:top-[12.3vh] h-[2px] md:left-[max(-2vw,-1%)] md:rotate-[17deg] left-[-15dvw] rotate-[70deg] top-[7dvh]"
         style={{ background: 'linear-gradient(90deg, #007AFF 0%, #FA8F21 100%)', width: "clamp(30vw, 40vw, 45vw)" }}
       ></div>
 
       <div
-        className="absolute top-[12.3vh] h-[2px] right-[max(-2vw,-1%)] rotate-[-17deg]"
+        className="absolute md:top-[12.3vh] h-[2px] md:right-[max(-2vw,-1%)] md:rotate-[-17deg] right-[-15dvw] rotate-[-70deg] top-[7dvh]"
         style={{ background: 'linear-gradient(90deg, #007AFF 0%, #FA8F21 100%)', width: "clamp(40vw, 40vw, 45vw)" }}
       ></div>
 
       <div
-        className="absolute bottom-[15.5vh] h-[2px] left-[max(-2vw,-2.5%)] rotate-[-22.5deg]"
+        className="absolute md:bottom-[15.5vh] h-[2px] md:left-[max(-2vw,-2.5%)] md:rotate-[-22.5deg] left-[-15dvw] rotate-[-65deg] bottom-[7dvh]"
         style={{ background: 'linear-gradient(90deg, #007AFF 0%, #FA8F21 100%)', width: "clamp(40vw, 40vw, 45vw)" }}
       ></div>
 
       <div
-        className="absolute bottom-[15.5vh] bg-white h-[2px] right-[max(-2vw,-2.5%)] rotate-[22.5deg]"
+        className="absolute md:bottom-[15.5vh] bg-white h-[2px] md:right-[max(-2vw,-2.5%)] md:rotate-[22.5deg] right-[-15dvw] rotate-[65deg] bottom-[7dvh]"
         style={{ background: 'linear-gradient(90deg, #007AFF 0%, #FA8F21 100%)', width: "clamp(40vw, 40vw, 45vw)" }}
       ></div>
 
       <div className="w-full h-max flex justify-center">
-        <h1 className="bg-gradient-to-r from-[#007AFF] to-[#FA8F21] bg-clip-text text-transparent text-5xl">
+        <h1 className="bg-gradient-to-r from-[#007AFF] to-[#FA8F21] bg-clip-text text-transparent text-2xl md:text-5xl">
           THE TESA Framework
         </h1>
       </div>
 
-      <div className="h-max mt-4 w-[40%] gap-4 flex flex-wrap justify-between z-10">
+      <div className="h-max mt-4 border w-[80%] md:w-[40%] gap-y-4 flex flex-wrap justify-between z-10">
         {FrameworkObject.map((item, index) => (
           <motion.div
             initial = {{opacity: 0, scale: 0}}
@@ -45,7 +45,7 @@ export const Framework = () => {
             transition={{duration: 0.8, delay: item.id / 10}}
             viewport={{once: true}}
             key={index}
-            className={`w-[30%] h-[150px] flex justify-center items-center ${
+            className={`w-[30%] h-[90px] md:h-[150px] flex justify-center items-center ${
               item.id === 5 ? 'bg-transparent rounded-full' : 'bg-white rounded-md'
             }`}
           >
