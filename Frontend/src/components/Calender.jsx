26a75d1559d844c61ab2e4cd0d0a2755e937773e
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-28 10:27:35 +0100

    Refactor Calender component for improved responsiveness and styling

--- Frontend/src/components/Calender.jsx
+++ Frontend/src/components/Calender.jsx
@@ -7,13 +7,13 @@ import CustomDateCalendar from './UI/CalenderCard';
 
 export const Calender = () => {
   return (
-    <div className='w-full h-screen flex relative items-end'>
+    <div className='w-full h-[120dvh] md:h-screen flex relative items-end'>
         <DynamicBackground imageUrl={Background}/>
-        <div className='w-full px-8 h-[90%] flex z-10'>
-            <div className='h-full flex justify-end items-center px-6 w-[40%] '>
+        <div className='w-full px-2 md:px-8 h-[80%] md:h-[90%] flex z-10 md:flex-row flex-col'>
+            <div className=' h-[40%] md:h-full flex justify-center md:justify-end items-center md:px-6 w-full md:w-[40%] '>
                 <img src={CalenderImage} alt="" className='w-[100%] h-full object-contain bo'/>
             </div>
-            <div className='h-full flex justify-start items-center px-6 w-[60%]'>
+            <div className='h-[60%] md:h-full flex justify-center md:justify-start items-center px-6 w-full md:w-[60%]'>
               <CustomDateCalendar/>
             </div>
         </div>
