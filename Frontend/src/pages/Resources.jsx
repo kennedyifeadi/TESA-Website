dec7b53e4127cd4f4f6589eaf4bca73745ced3f6
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-31 15:57:56 +0100

    Refactor Resources page to add ResourceLanding, FilterResources, and FilteredResources components

--- Frontend/src/pages/Resources.jsx
+++ Frontend/src/pages/Resources.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { ResourceLanding } from '../components/ResourceLanding'
+import { FilterResources } from '../components/FilterResources'
+import { FilteredResouces } from '../components/FilteredResouces'
 
 export const Resources = () => {
   return (
-    <div>Resources</div>
+    <div className='w-full h-max flex flex-col'>
+      <ResourceLanding/>
+      <FilterResources/>
+      <FilteredResouces/>
+    </div>
   )
 }
