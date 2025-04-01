8620aa039d063201da18df60c442dd997d763e44
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-02 00:36:34 +0100

    Refactor FilterResources component to add student image and improve styling

--- Frontend/src/components/FilteredResouces.jsx
+++ Frontend/src/components/FilteredResouces.jsx
@@ -3,6 +3,7 @@ import Note from "../assets/gifs/LectureNotes.gif"
 import VideoNote from "../assets/gifs/VideoNotes.gif"
 import PQs from "../assets/gifs/PastQuestions.gif"
 import Textbook from "../assets/gifs/Textbooks.gif"
+import Student from "../assets/gifs/Student.gif"
 import { ResoucesContext } from '../context/ResoucesContext'
 
 export const FilteredResouces = () => {
@@ -31,15 +32,15 @@ export const FilteredResouces = () => {
     }
   ]
   return (
-    <div className='w-full h-screen flex bg-[#000000] flex-col'>
+    <div className='w-full h-screen flex justify-between bg-[#000000] flex-col'>
       <div className='w-full flex justify-center flex-col text-white mt-4'>
-        <h1 className='w-full text-center text-4xl'>{Department}</h1>
-        <span className='w-full text-center text-xl mt-2'>{level}</span>
+        <h1 className='w-full text-center text-2xl md:text-4xl'>{Department}</h1>
+        <span className='w-full text-center text-lg md:text-xl mt-2'>{level}</span>
       </div>
-      <div className='w-full flex justify-evenly flex-wrap h-[50%] items-center'>
+      <div className='w-full flex justify-evenly flex-wrap gap-y-4 h-[50%] items-center'>
         {
           Resources.map((resource, index)=>(
-            <div key={index} className='group relative w-[30%] max-w-[200px] h-[200px] bg-[#392B1A] border-white border rounded-md cursor-pointer active:scale-90 duration-500 transition-all ease-in-out'>
+            <div key={index} className='group relative w-[150px] h-[150px] md:w-[30%] md:min-w-[180px] md:max-w-[200px] md:h-[200px] bg-[#392B1A] border-white border rounded-md cursor-pointer active:scale-90 duration-500 transition-all ease-in-out'>
               <div className="absolute inset-0 bg-gradient-to-r from-[#007AFF] to-[#FA8F21] opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100 rounded-md z-0"></div>
               <a href={resource.link} onClick={(e) => {e.stopPropagation(); window.open(resource.link, "_blank");}} target='_blank' rel="noopener noreferrer" className='relative w-full h-full flex-col flex z-10'>
                 <div className='w-full h-[80%]'>
@@ -51,7 +52,9 @@ export const FilteredResouces = () => {
           ))
         }
       </div>
-      <div className='w-full flex justify-center h-[30%] items-center'></div>
+      <div className='w-full flex justify-center h-[20%] md:h-[30%] items-center'>
+        <img src={Student} alt="" className=' h-full'/>
+      </div>
     </div>
   )
 }
