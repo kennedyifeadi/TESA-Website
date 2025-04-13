27070be240fbcc94bfce6480ec8f173e9d2a8681
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-13 13:51:25 +0100

    Refactor motion effects in Resources page

--- Frontend/src/pages/Resources.jsx
+++ Frontend/src/pages/Resources.jsx
@@ -3,20 +3,16 @@ import { ResourceLanding } from '../components/ResourceLanding'
 import { FilterResources } from '../components/FilterResources'
 import { FilteredResouces } from '../components/FilteredResouces'
 import { ResoucesContextProvider } from '../context/ResoucesContext'
-import { motion } from 'framer-motion' 
 
 export const Resources = () => {
   return (
-    <motion.div
-    initial = {{x:300}}
-    animate = {{x:0}}
-    transition={{duration: 1}}
+    <div
      className='w-full h-max flex flex-col'>
       <ResourceLanding/>
       <ResoucesContextProvider>
         <FilterResources/>
         <FilteredResouces/>
       </ResoucesContextProvider>
-    </motion.div>
+    </div>
   )
 }
