1104c78f6a5a68d45102facda6206754268d7ddf
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-13 13:51:06 +0100

    Refactor SponsorshipHeroSection component to add motion effects to image and text

--- Frontend/src/components/SponsorshipHeroSection.jsx
+++ Frontend/src/components/SponsorshipHeroSection.jsx
@@ -3,6 +3,8 @@ import { DynamicBackground } from "./DynamicBackground";
 import Background from "../assets/images/HeroSectionBackground.jpg";
 import DummyBG from "../assets/images/dummyBG.jpg";
 import Cuate from '../assets/images/cuate.png'
+import { motion } from 'framer-motion'
+
 
 
 export const SponsorshipHeroSection = () => {
@@ -10,14 +12,22 @@ export const SponsorshipHeroSection = () => {
     <div className='w-full h-screen flex items-end px-2 md:px-8 relative  z-10'>
         <DynamicBackground imageUrl={Background}/>
         <div className='w-full h-[80%] md:h-[90%] pt-4 z-10'>
-            <div className='w-full h-[18%] md:h-[30%] relative flex flex-col justify-center items-center'>
+            <motion.div
+                initial = {{x:300}}
+    animate = {{x:0}}
+    transition={{duration: 1}}
+             className='w-full h-[18%] md:h-[30%] relative flex flex-col justify-center items-center'>
                 <DynamicBackground imageUrl={DummyBG}/>
                 <h1 className='w-full text-center text-2xl md:text-5xl text-white z-10' style={{ fontFamily: '"Aldrich", sans-serif' }}>SPONSORSHIP</h1>
                 <p className='w-full text-center text-[10px] md:text-md text-white z-10' style={{ fontFamily: '"Poppins", sans-serif' }}>Support us in driving progress and Making a Difference with TESA</p>
-            </div>
-            <div className='w-full h-[82%] md:h-[70%] flex justify-center items-center'>
+            </motion.div>
+            <motion.div
+                initial = {{x:-300}}
+    animate = {{x:0}}
+    transition={{duration: 1}}
+             className='w-full h-[82%] md:h-[70%] flex justify-center items-center'>
                 <img src={Cuate} alt="" className='md:w-[300px] w-[400px] h-[400px] md:h-[300px] object-contain' />
-            </div>
+            </motion.div>
         </div>
     </div>
   )
