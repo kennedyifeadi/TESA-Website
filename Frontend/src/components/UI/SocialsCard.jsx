f3984ba140981ce368ce7883354d67f044967752
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-13 11:33:22 +0100

    Refactor SocialsCard component to add link functionality

--- Frontend/src/components/UI/SocialsCard.jsx
+++ Frontend/src/components/UI/SocialsCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-export const SocialsCard = ({text, icon, hoverIcon}) => {
+export const SocialsCard = ({text, icon, hoverIcon, link}) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
@@ -8,7 +8,7 @@ export const SocialsCard = ({text, icon, hoverIcon}) => {
     onMouseEnter={() => setIsHovered(true)}
     onMouseLeave={() => setIsHovered(false)}
     >
-      <span className={`${isHovered ? "translate-y-[-20px] text-white" : "translate-y-0"} text-black text-[10px] md:text-2xl font-medium duration-500 ease-in-out transition-all`} style={{fontFamily:'"Poppins", sans-serif'}}>{text}</span>
+      <span className={`${isHovered ? "translate-y-[-20px] text-white" : "translate-y-0"} text-black text-[10px] md:text-2xl font-medium duration-500 ease-in-out transition-all`} style={{fontFamily:'"Poppins", sans-serif'}}> <a href={link}>{text}</a></span>
       {
         isHovered ? (
           <img src={hoverIcon} alt="" className='w-4 md:w-8 h-4 md:h-8 translate-y-[-10px]'/>
