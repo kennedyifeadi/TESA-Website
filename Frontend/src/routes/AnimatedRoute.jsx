f44d1e1b3307fc9f11c0d8ae2c27cf3081a714e1
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-14 00:28:35 +0100

    Refactor AnimatedRoutes component to improve code formatting and readability

--- Frontend/src/routes/AnimatedRoute.jsx
+++ Frontend/src/routes/AnimatedRoute.jsx
@@ -1,5 +1,5 @@
-import { Route, Routes, useLocation } from "react-router-dom"
-import { AnimatePresence } from "framer-motion"
+import { Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import { LandingPage } from "../pages/LandingPage";
 import { Sponsorships } from "../pages/Sponsorships";
 import { Resources } from "../pages/Resources";
@@ -9,20 +9,20 @@ import { PageNotFound } from "../pages/PageNotFound";
 import { Updates } from "../pages/Updates";
 
 export const AnimatedRoutes = () => {
-    const location = useLocation();
-      return (
-          <div className="sm:h-full sm:w-full">
-          <AnimatePresence>
-              <Routes location={location} key={location.pathname}>
-                  <Route path="/" element={<LandingPage/>} exact />
-                  <Route path="/sponsorship" element={<Sponsorships/>} />
-                  <Route path="/updates" element={<Updates/>} />
-                  <Route path="/resources" element={<Resources/>} />
-                  <Route path="/payments" element={<Payments/>} />
-                  <Route path="/contact" element={<Contact/>} />
-                  <Route path="*" element={<PageNotFound/>} /> 
-              </Routes>
-        </AnimatePresence>
-        </div>
-      )
-}
+  const location = useLocation();
+  return (
+    <div className="sm:h-full sm:w-full">
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<LandingPage />} exact />
+          <Route path="/sponsorship" element={<Sponsorships />} />
+          <Route path="/updates" element={<Updates />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/payments" element={<Payments />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </AnimatePresence>
+    </div>
+  );
+};
