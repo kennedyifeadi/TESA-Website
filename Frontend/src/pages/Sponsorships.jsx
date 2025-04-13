6c810d7fa8fe37bc75fe0ffcc302799bd6fb4872
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-13 13:51:37 +0100

    Refactor motion effects in Sponsorships page

--- Frontend/src/pages/Sponsorships.jsx
+++ Frontend/src/pages/Sponsorships.jsx
@@ -3,19 +3,15 @@ import { SponsorshipHeroSection } from '../components/SponsorshipHeroSection'
 import { WhySponsor } from '../components/WhySponsor'
 import { Sponsors } from '../components/Sponsors'
 import { BecomeASponsor } from '../components/BecomeASponsor'
-import { motion } from "framer-motion"
 
 export const Sponsorships = () => {
   return (
-    <motion.div 
-    initial = {{x:300}}
-    animate = {{x:0}}
-    transition={{duration: 1}}
+    <div     
     className='w-full h-max flex flex-col relative'>
       <SponsorshipHeroSection/>
       <WhySponsor/>
       <Sponsors/>
       <BecomeASponsor/>
-    </motion.div>
+    </div>
   )
 }
