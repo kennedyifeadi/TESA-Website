d8ee5b3a47eec6f14e60ddb96a80ed489be1813d
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-13 13:51:57 +0100

    Refactor motion effects in Updates page

--- Frontend/src/pages/Updates.jsx
+++ Frontend/src/pages/Updates.jsx
@@ -2,18 +2,15 @@ import React from 'react'
 import { Calender } from '../components/Calender'
 import { Announcements } from '../components/Announcements'
 import { Gallery } from '../components/Gallery'
-import { motion } from 'framer-motion'
 
 export const Updates = () => {
   return (
-    <motion.div
-    initial = {{x:300}}
-    animate = {{x:0}}
-    transition={{duration: 1}}
+    <div
+  
     className='w-full flex flex-col relative h-max'>
       <Calender/>
       <Announcements/>
       <Gallery/>
-    </motion.div>
+    </div>
   )
 }
