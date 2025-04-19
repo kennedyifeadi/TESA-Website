79a350e2bfd9ce61d9fa65a28c20cb1e3cb7ea01
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-19 13:43:12 +0100

    Refactor Admin components to update layout structure and add responsive styling

--- Frontend/src/components/Admin/Dashboard.jsx
+++ Frontend/src/components/Admin/Dashboard.jsx
@@ -5,7 +5,10 @@ export const Dashboard = () => {
   return (
     <div className='w-full h-full relative'>
       <div className='relative w-full h-full opacity-15' style={{ backgroundImage: `url(${TESA})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}></div>
-      <div className='relative'></div>
+      <div className='relative flex flex-col w-full h-full pt-14 px-6 gap-6'>
+        <div className='w-full flex h-[60px]'></div>
+        <div className='w-full flex h-max'></div>
+      </div>
     </div>
   )
 }
