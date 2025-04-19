34e798664b45e7169749e032f464dfe6db00e0b9
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-19 13:25:51 +0100

    Refactor Adverts component to add dynamic background image

--- Frontend/src/components/Admin/Adverts.jsx
+++ Frontend/src/components/Admin/Adverts.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import TESA from "../../assets/images/logo.png"
+
 
 export const Adverts = () => {
   return (
-    <div>Adverts</div>
+    <div className='w-full h-full relative'>
+      <div className='relative w-full h-full opacity-15' style={{ backgroundImage: `url(${TESA})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}></div>
+      <div className='relative'></div>
+    </div>
   )
 }
