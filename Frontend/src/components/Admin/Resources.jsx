c1d3fa1268d804d618222b325781c985938ee533
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-19 13:26:03 +0100

    Refactor Admin components to add dynamic background image

--- Frontend/src/components/Admin/Resources.jsx
+++ Frontend/src/components/Admin/Resources.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import TESA from "../../assets/images/logo.png"
+
 
 export const AdminResources = () => {
   return (
-    <div>Resources</div>
+    <div className='w-full h-full relative'>
+      <div className='relative w-full h-full opacity-15' style={{ backgroundImage: `url(${TESA})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}></div>
+      <div className='relative'></div>
+    </div>
   )
 }
