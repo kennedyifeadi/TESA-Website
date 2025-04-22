996462ab1315a9ff0d51ff2ee4eba0a8822078b3
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-22 06:36:37 +0100

    Refactor component structure and add new Field component

--- Frontend/src/components/Admin/ExcoField.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ExcoField = () => {
-  return (
-    <div>ExcoField</div>
-  )
-}
---
+++ Frontend/src/components/Admin/Field.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Field = ({name, position, level, email, phone, twitter, instagram, image}) => {
+  return (
+    <div className='w-full h-[40px] flex items-center before:absolute relative before:bottom-0 before:w-full before:h-[1px] before:bg-gradient-to-r before:from-[#007AFF] before:to-[#FA8F21]'>This is the filed</div>
+  )
+}
