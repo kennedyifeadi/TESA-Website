84b13d0a2672ec992d0abdc3b8e00054533cb4ff
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-23 23:50:13 +0100

    Refactor Field component to add additional input fields and improve layout structure

--- Frontend/src/components/Admin/Field.jsx
+++ Frontend/src/components/Admin/Field.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
-export const Field = ({name, position, level, email, phone, twitter, instagram, image}) => {
+export const Field = ({name, position, level, email, phone, twitter, instagram, image, description, subtext, date, time, category}) => {
+  const fieldDetails = [name, position || description || subtext, level, email || date, phone || time, twitter || category, instagram, image]
   return (
-    <div className='w-full h-[40px] flex items-center before:absolute relative before:bottom-0 before:w-full before:h-[1px] before:bg-gradient-to-r before:from-[#007AFF] before:to-[#FA8F21]'>This is the filed</div>
+    <div className='w-full h-[40px] flex items-center before:absolute relative before:bottom-0 before:w-full before:h-[1px] before:bg-gradient-to-r before:from-[#007AFF] before:to-[#FA8F21]'>
+      <div className='w-[96%] h-full'>
+        {
+          fieldDetails.map((details, index) =>(
+            <input type={index === 7 ? "text" : "image"} placeholder={details} />
+          ))
+        }
+      </div>
+      <div className='w-[50px] h-full'></div>
+    </div>
   )
 }
