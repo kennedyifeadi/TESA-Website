f7320266af53505dc7993e792309c16fa1fde0dc
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-24 01:08:26 +0100

    Refactor AdminDashboard component to add dynamic titles and positions

--- Frontend/src/components/Admin/Dashboard.jsx
+++ Frontend/src/components/Admin/Dashboard.jsx
@@ -4,6 +4,7 @@ import { Field } from './Field'
 
 export const Dashboard = () => {
   const titles = ['Name', 'Position', 'Level', 'Email', 'Phone no', 'Twitter', 'Instagram','Image']
+  const positions = ["president", "vice president", "General Secretary", "A.General Secretary", "P.Relations Officer", "Social Director", "Financial Secretary", "Sports Director", "Tresurer"]
   return (
     <div className='w-full h-full relative'>
       <div className='absolute top-0 w-full h-full opacity-15' style={{ backgroundImage: `url(${TESA})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}></div>
@@ -20,8 +21,11 @@ export const Dashboard = () => {
         <div className='w-full flex flex-col h-max before:absolute relative before:right-0 before:w-[1px] before:h-full before:bg-gradient-to-b before:from-[#007AFF] before:to-[#FA8F21]
         after:absolute after:left-0 after:w-[1px] after:h-full after:bg-gradient-to-b after:from-[#007AFF] after:to-[#FA8F21]
         '>
-          <Field />
-          {/* <span className='w-[100px] overflow-hidden text-ellipsis whitespace-nowrap'>kennedy ifeadi chukwunwike</span> */}
+          {
+            positions.map((position, index) => (
+              <Field name={"Name"} email={"Email"} position={position} twitter={"Twitter"} instagram={"Instagram"} level={"Level"} phone={"Phone no"}/>
+            ))
+          }
         </div>
       </div>
     </div>
