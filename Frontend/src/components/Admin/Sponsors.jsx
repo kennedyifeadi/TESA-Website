cc518a5f8546ce963465c54a7be78e8f1c09f4fc
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-25 11:46:42 +0100

    Refactor AdminSponsors component to add new sponsor fields and functionality

--- Frontend/src/components/Admin/Sponsors.jsx
+++ Frontend/src/components/Admin/Sponsors.jsx
@@ -1,23 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TESA from "../../assets/images/logo.png"
-
+import { Field } from './Field'
 
 export const AdminSponsors = () => {
-  const titles = ['Name', 'Position', 'Text','Image']
+  const titles = ['Name', 'Position', 'Text', 'Image']
+  
+  // State to track sponsors
+  const [sponsors, setSponsors] = useState([
+    { id: 1, name: "Name", position: "Position", text: "Text", image: true }
+  ]);
+  
+  // Function to add a new sponsor
+  const addNewSponsor = () => {
+    const newId = sponsors.length > 0 ? Math.max(...sponsors.map(sponsor => sponsor.id)) + 1 : 1;
+    setSponsors([...sponsors, { 
+      id: newId, 
+      name: "Name", 
+      position: "Position", 
+      text: "Text", 
+      image: true 
+    }]);
+  }
+
   return (
     <div className='w-full h-full relative'>
       <div className='absolute top-0 w-full h-full opacity-15' style={{ backgroundImage: `url(${TESA})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}></div>
       <div className='relative flex flex-col w-full h-full pt-14 px-6 gap-6'>
-        <div className='w-full flex h-[70px] border-b border-b-black  bg-gradient-to-r from-[#007AFF] to-[#FA8F21]'>
+        <div className='w-full flex h-[70px] border-b border-b-black bg-gradient-to-r from-[#007AFF] to-[#FA8F21]'>
           {
             titles.map((title, index) => (
               <div key={index} className={`flex-1 flex justify-center items-center h-full ${index === 3 ? "border-r-none" : "border-r border-r-black"}`}>
-                <h1 className=' text-white cursor-default'>{title}</h1>
+                <h1 className='text-white cursor-default'>{title}</h1>
               </div>
             ))
           }
         </div>
-        <div className='w-full flex h-max'>klklflkefkl</div>
+        
+        {/* Sponsor Fields */}
+        <div className='w-full flex flex-col gap-4'>
+          {
+            sponsors.map((sponsor) => (
+              <Field 
+                key={sponsor.id}
+                name={sponsor.name} 
+                position={sponsor.position} 
+                text={sponsor.text}
+                image={sponsor.image}
+                endpoint={"postSponsor"}
+              />
+            ))
+          }
+        </div>
+        
+        {/* Add New Sponsor Button */}
+        <div 
+          onClick={addNewSponsor} 
+          className='w-full h-[50px] border border-[#8080809f] cursor-pointer active:scale-90 duration-500 ease-in-out rounded-lg flex justify-center items-center bg-[#ffffffc2]'
+        >
+          Add New Sponsor
+        </div>
       </div>
     </div>
   )
