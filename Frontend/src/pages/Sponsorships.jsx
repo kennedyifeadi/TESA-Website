64f11f3c97e7e1145434a42a38e060121d45e8ec
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-05-22 02:21:38 +0100

    Add smooth scrolling functionality to Sponsorships page with onScrollClick prop in SponsorshipHeroSection

--- Frontend/src/pages/Sponsorships.jsx
+++ Frontend/src/pages/Sponsorships.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { SponsorshipHeroSection } from '../components/SponsorshipHeroSection'
 import { WhySponsor } from '../components/WhySponsor'
 import { Sponsors } from '../components/Sponsors'
 import { BecomeASponsor } from '../components/BecomeASponsor'
 
 export const Sponsorships = () => {
+  const sponsorRef = useRef(null);
+
+  const scrollToSponsorSection = () => {
+    sponsorRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
   return (
     <div     
     className='w-full h-max flex flex-col relative'>
-      <SponsorshipHeroSection/>
+      <SponsorshipHeroSection onScrollClick={scrollToSponsorSection}/>
       <WhySponsor/>
       <Sponsors/>
-      <BecomeASponsor/>
+      <div ref={sponsorRef}>
+        <BecomeASponsor />
+      </div>
     </div>
   )
 }
