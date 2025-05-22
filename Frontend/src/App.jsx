e4242f7e891aae3d5149e9ffafc81f10f461e9e0
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-05-22 02:21:20 +0100

    Add ScrollToTop component to App for improved navigation experience

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,10 +2,13 @@ import { BrowserRouter} from "react-router-dom";
 import { NavContextProvider } from "./context/NavContext";
 import { AuthProvider } from "./context/AuthProvider";
 import { AppWrapper } from "./routes/MainRoutes";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+      {/* Wrapping the App with NavContextProvider and AuthProvider */}
       <NavContextProvider>
         <AuthProvider>
           <AppWrapper />
